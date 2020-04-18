23051955369b1e5550423db4474b9d435c43d5d4
limjun92 <limjun9205@naver.com>
2020-04-18 18:15:09 +0900

    papagoAPI

--- front_end/src/components/main.vue
+++ front_end/src/components/main.vue
@@ -1,13 +1,27 @@
 <template>
   <div>
     <b-form inline>
+      <div>
       <label class="sr-only" for="inlineFormInputName2">Name</label>
       <b-input class="mb-2 mr-sm-2 mb-sm-0" id="inlineFormInputName2" v-model="episodes"  />
       <b-input class="mb-2 mr-sm-2 mb-sm-0" id="inlineFormInputName2" v-model="result"  />
       <b-button variant="primary" v-on:click="login">콘솔 확인</b-button>
+      </div>
+    </b-form>
+    <b-form inline>
+      <div>
       <b-input class="mb-2 mr-sm-2 mb-sm-0" id="inlineFormInputName2" v-model="x"  />
       <b-input class="mb-2 mr-sm-2 mb-sm-0" id="inlineFormInputName2" v-model="y"  />
       <b-button variant="primary" v-on:click="post">콘솔 확인</b-button>
+      </div>
+    </b-form>
+    <b-form inline>
+      <div>
+      <!-- <b-input class="mb-2 mr-sm-2 mb-sm-0" id="inlineFormInputName2" v-model="before"  /> -->
+      <textarea cols="40" rows="10" autofocus required wrap="hard" v-model="before">??</textarea>
+      <b-button variant="primary" v-on:click="trans">콘솔 확인</b-button>
+      <div>{{after}}</div>
+      </div>
     </b-form>
   </div>
 </template>
@@ -23,6 +37,8 @@ export default {
       result: '',
       x: '',
       y: '',
+      before: '',
+      after: '',
       credentials: {
         username: '',
         password: ''
@@ -31,10 +47,16 @@ export default {
     }
   },
   methods: {
-    post: function(){
-      common.post('/post',this.x)
+    post: function () {
+      common.post('/post', this.x)
         .then((res) => {
           this.y = res.data
+        })
+    },
+    trans: function () {
+      common.post('/trans', this.before)
+        .then((res) => {
+          this.after = res.data
         })
     },
     login: function () {
